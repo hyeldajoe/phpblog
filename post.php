8f40d32929a4e14dd8b3e6889f55089926bc3693
Hyelda Joseph Fomnya <hyeldaj@gmail.com>
2022-04-17 17:22:10 +0100

    First Commit

---
+++ post.php
@@ -0,0 +1,16 @@
+<?php include "includes/header.php"; ?>
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">PHP International Conference</h2>
+            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+
+            <p>Vivamus laoreet. Vivamus in erat ut urna cursus vestibulum. In auctor lobortis lacus. Ut a nisl id ante tempus hendrerit. Fusce neque.
+
+Praesent egestas tristique nibh. Curabitur at lacus ac velit ornare lobortis. Proin magna. Nam commodo suscipit quam. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.</p>
+ <p>Fusce convallis metus id felis luctus adipiscing. Etiam ultricies nisi vel augue. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Aliquam eu nunc. Ut non enim eleifend felis pretium feugiat.
+
+Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Etiam vitae tortor. Vestibulum volutpat pretium libero. Nam adipiscing. Vestibulum fringilla pede sit amet augue.</p>   
+</div><!-- /.blog-post -->
+
+
+<?php include "includes/footer.php"; ?> 
---
+++ posts.php
@@ -0,0 +1,23 @@
+<?php include "includes/header.php"; ?>
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">PHP International Conference</h2>
+            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+
+            <p>Vivamus laoreet. Vivamus in erat ut urna cursus vestibulum. In auctor lobortis lacus. Ut a nisl id ante tempus hendrerit. Fusce neque.
+
+Praesent egestas tristique nibh. Curabitur at lacus ac velit ornare lobortis. Proin magna. Nam commodo suscipit quam. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.</p>
+   <a class="readmore" href="post.php?id=1">Read More</a>      
+</div><!-- /.blog-post -->
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">How to code in PHP</h2>
+            <p class="blog-post-meta">September 4, 2021 by <a href="#">John</a></p>
+
+            <p>Vivamus laoreet. Vivamus in erat ut urna cursus vestibulum. In auctor lobortis lacus. Ut a nisl id ante tempus hendrerit. Fusce neque.
+
+Praesent egestas tristique nibh. Curabitur at lacus ac velit ornare lobortis. Proin magna. Nam commodo suscipit quam. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.</p>
+<a class="readmore" href="post.php?id=1">Read More</a>                
+</div><!-- /.blog-post -->
+
+<?php include "includes/footer.php"; ?> 
